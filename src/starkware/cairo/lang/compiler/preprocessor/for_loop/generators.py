3319d0cfa74106cadf5fe8dcdd7bb9a1a1288b91
Marek Kaput <marek.kaput@swmansion.com>
2022-05-06 11:04:30 +0200

    WIP: implement slice lowering

--- src/starkware/cairo/lang/compiler/preprocessor/for_loop/generators.py
+++ src/starkware/cairo/lang/compiler/preprocessor/for_loop/generators.py
@@ -5,7 +5,11 @@ from starkware.cairo.lang.compiler.ast.bool_expr import BoolExpr
 from starkware.cairo.lang.compiler.ast.cairo_types import CairoType
 from starkware.cairo.lang.compiler.ast.code_elements import CodeBlock
 from starkware.cairo.lang.compiler.ast.expr import Expression, ExprIdentifier
-from starkware.cairo.lang.compiler.ast.for_loop import ForClauseIn, ForGeneratorRange
+from starkware.cairo.lang.compiler.ast.for_loop import (
+    ForClauseIn,
+    ForGeneratorRange,
+    ForGeneratorSlice,
+)
 
 
 class GeneratorLowering(ABC):
@@ -35,13 +39,19 @@ class GeneratorLowering(ABC):
 
     @staticmethod
     def from_in_clause(clause: ForClauseIn) -> "GeneratorLowering":
+        # We are importing locally to prevent import cycles
         from starkware.cairo.lang.compiler.preprocessor.for_loop.range_generator import (
             RangeGeneratorLowering,
+        )
+        from starkware.cairo.lang.compiler.preprocessor.for_loop.slice_generator import (
+            SliceGeneratorLowering,
         )
 
         generator = clause.generator
         if isinstance(generator, ForGeneratorRange):
             return RangeGeneratorLowering(generator)
+        elif isinstance(generator, ForGeneratorSlice):
+            return SliceGeneratorLowering(generator)
         else:
             raise NotImplementedError(
                 f"Lowering '{generator.func_ident.name}' generator is not implemented yet."
---
+++ src/starkware/cairo/lang/compiler/preprocessor/for_loop/slice_generator.py
@@ -0,0 +1,91 @@
+from typing import Tuple
+
+from starkware.cairo.lang.compiler.ast.bool_expr import BoolExpr
+from starkware.cairo.lang.compiler.ast.cairo_types import CairoType, TypeFelt, TypePointer
+from starkware.cairo.lang.compiler.ast.code_elements import CodeBlock
+from starkware.cairo.lang.compiler.ast.expr import (
+    Expression,
+    ExprConst,
+    ExprAssignment,
+    ExprIdentifier,
+    ExprOperator,
+)
+from starkware.cairo.lang.compiler.ast.for_loop import ForGeneratorSlice
+from starkware.cairo.lang.compiler.error_handling import Location
+from starkware.cairo.lang.compiler.preprocessor.for_loop.errors import ForLoopLoweringError
+from starkware.cairo.lang.compiler.preprocessor.for_loop.generators import GeneratorLowering
+
+
+class SliceGeneratorLowering(GeneratorLowering):
+    generator_location: Location
+    array: Expression
+    number: Expression
+    size: Expression
+
+    def __init__(self, generator: ForGeneratorSlice):
+        self.generator_location = generator.location
+
+        args = generator.arguments.args
+
+        # Validate arguments.
+        if len(args) < 2:
+            raise ForLoopLoweringError(
+                "Slice generator expects at least 'array' and 'number' arguments.",
+                location=generator.location,
+            )
+
+        if len(args) > 3:
+            assert args[3].location is not None and args[-1].location is not None
+            excessive_args_location = args[3].location.span(args[-1].location)
+
+            raise ForLoopLoweringError(
+                "Too many arguments passed to slice generator.", location=excessive_args_location
+            )
+
+        # TODO(mkaput, 21/04/2022): Support keyword arguments here.
+        #   There is a refactoring opportunity here, as this functionality
+        #   is implemented in process_expr_assignment_list in Preprocessor.
+        #   As for now, we just error proactively.
+        for arg in args:
+            if arg.identifier is not None:
+                raise ForLoopLoweringError(
+                    "Keyword arguments are not supported here yet.", location=arg.location
+                )
+
+        # Pad arguments to 3 element list with None as filling. This will simplify further code.
+        if len(args) < 3:
+            args = [*args, None]
+
+        # Extract arguments and set defaults
+        [array, number, size] = args
+
+        assert isinstance(array, ExprAssignment)
+        self.array = array.expr
+
+        assert isinstance(number, ExprAssignment)
+        self.number = number.expr
+
+        if size is None:
+            self.size = ExprConst(val=1)
+        else:
+            assert isinstance(size, ExprAssignment)
+            self.size = size.expr
+
+    def iterator_type(self) -> CairoType:
+        return TypePointer(
+            TypeFelt(location=self.generator_location), location=self.generator_location
+        )
+
+    def init_envelope_iterator(self) -> Tuple[CodeBlock, Expression]:
+        # return CodeBlock([]), self.array
+        raise NotImplementedError
+
+    def condition(self, iter_expr: ExprIdentifier) -> Tuple[CodeBlock, BoolExpr]:
+        # return CodeBlock([]), BoolExpr(
+        #     eq=True, a=iter_expr, b=self.number, location=self.generator_location
+        # )
+        raise NotImplementedError
+
+    def increment_iterator(self, iter_expr: ExprIdentifier) -> Tuple[CodeBlock, Expression]:
+        # return CodeBlock([]), ExprOperator(op="+", a=iter_expr, b=self.size)
+        raise NotImplementedError
