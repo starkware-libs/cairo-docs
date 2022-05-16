72b48f1b8f8079ce5963c6e5b2100b9400306293
Marek Kaput <marek.kaput@swmansion.com>
2022-05-16 09:39:36 +0200

    address nitpicks

--- src/starkware/cairo/lang/compiler/preprocessor/code_element_injecting_visitor.py
+++ src/starkware/cairo/lang/compiler/preprocessor/code_element_injecting_visitor.py
@@ -13,11 +13,15 @@ from starkware.cairo.lang.compiler.ast.visitor import Visitor
 
 
 @dataclasses.dataclass
-class CodeElementsInjection:
+class CodeElementInjection:
+    """
+    A list of code elements to be injected.
+    """
+
     code_elements: Iterable[CommentedCodeElement]
 
     @classmethod
-    def from_code_block(cls, code_block: CodeBlock) -> "CodeElementsInjection":
+    def from_code_block(cls, code_block: CodeBlock) -> "CodeElementInjection":
         return cls(code_block.code_elements)
 
 
@@ -30,22 +34,29 @@ class CodeElementInjectingVisitor(ABC, Visitor):
 
     * Add new functions to generated Cairo code, by calling ``inject_function`` inside visitor
       methods. This visitor will try to put these new functions as close as possible to source
-      code location, that is: in current ``CairoModule``, right after currently processed
+      code location, that is: in the current ``CairoModule``, right after the currently processed
       ``CodeElementFunction``.
 
     For example usage check out ``ForLoopLoweringVisitor.visit_CodeElementFor``.
     """
 
-    _injection_scopes: List[List[CodeElementFunction]]
-
     def __init__(self):
         super().__init__()
-        self._injection_scopes = []
+
+        # An injection scope is a list of functions to be added to the AST upon finishing visiting
+        # a Cairo module or any other meaningful elements.
+        #
+        # We support nested Cairo module here, so we store injection scopes in a stack.
+        self._injection_scopes: List[List[CodeElementFunction]] = []
 
     def inject_function(self, func: CodeElementFunction):
         """
-        Call from visitor method to register a new function to be added to current Cairo module
-        as close as possible to source code location where this visitor has been called.
+        Registers a new function to be added to the current Cairo module.
+
+        The function will be added as close as possible to the source code location where this
+        visitor has been called.
+
+        This method can only be called from within other visitor methods.
         """
         self._injection_scopes[-1].append(func)
 
@@ -58,15 +69,14 @@ class CodeElementInjectingVisitor(ABC, Visitor):
 
         code_elements = []
         for commented_code_elm in elm.code_elements:
-            visited_elms = self.visit(commented_code_elm.code_elm)
+            visited_elm = self.visit(commented_code_elm.code_elm)
 
             # Flatten lists of code elements if returned from visitor method.
-            if isinstance(visited_elms, CodeElementsInjection):
-                # This line forgets the comment assigned to commented_code_elm.
-                # This shouldn't matter in current use cases, but please be aware of this.
-                code_elements.extend(visited_elms.code_elements)
+            if isinstance(visited_elm, CodeElementInjection):
+                # Note that the following line ignores the comment assigned to commented_code_elm.
+                code_elements.extend(visited_elm.code_elements)
             else:
-                code_elements.append(dataclasses.replace(commented_code_elm, code_elm=visited_elms))
+                code_elements.append(dataclasses.replace(commented_code_elm, code_elm=visited_elm))
 
             # Inject any new functions if possible.
             if can_inject_functions:
@@ -87,4 +97,4 @@ class CodeElementInjectingVisitor(ABC, Visitor):
             yield
         finally:
             injected_functions = self._injection_scopes.pop()
-            assert not injected_functions, "There are leftover not injected functions!"
+            assert len(injected_functions) > 0, "Function injection failed to run completely."
