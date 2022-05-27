0b64398a2829fac2cb50510ed1800ca2a48c8b05
Marek Kaput <marek.kaput@swmansion.com>
2022-05-27 14:37:15 +0200

    support nested conjunction operations in if conditions

--- src/starkware/cairo/lang/compiler/preprocessor/bool_expr/lowering_test.py
+++ src/starkware/cairo/lang/compiler/preprocessor/bool_expr/lowering_test.py
@@ -33,8 +33,7 @@ end
 
 
 def test_lowers_nested_and_without_else():
-    verify_exception(
-        """
+    code = """
 func main():
     let a = 10
     let b = 12
@@ -44,12 +43,24 @@ func main():
     end
     ret
 end
-""",
-        """
-file:?:?: Nested and expressions are not supported yet.
-    if a == 10 and b == 12 and c == 14:
-       ^*****************^
-""",
+"""
+    assert (
+        lower_and_format(code)
+        == """\
+func main():
+    let a = 10
+    let b = 12
+    let c = 14
+    if a == 10:
+        if b == 12:
+            if c == 14:
+                let x = a + b + c
+            end
+        end
+    end
+    ret
+end
+"""
     )
 
 
